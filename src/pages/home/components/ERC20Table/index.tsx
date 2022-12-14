27ab2ec37ba24d0fd7a2bc502381d17e8599bcdf
Megan Zhang <megan@parallel.fi>
2022-12-14 11:43:55 +0800

    feat: get wallet balance of weth

--- src/pages/home/components/ERC20Table/index.tsx
+++ src/pages/home/components/ERC20Table/index.tsx
@@ -7,8 +7,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 import { TokenIcon, TokenName } from '@/components/tokenIcon';
+import { useWeb3Context } from '@/contexts/web3Context';
+import { Erc20__factory } from '@/contracts';
+import { WETH_ADDRESS } from '@/static/constants/contract';
 
 const IconWrap = styled(TableCell)`
   display: flex;
@@ -27,16 +32,28 @@ const StyledTableRow = styled(TableRow)(() => ({
 const createData = (
   name: TokenName,
   balance: number,
+  tvl: number,
   supplyAPY: number,
   borrowAPY: number,
-  Liquidity: number
+  liquidity: number
 ) => {
-  return { name, balance, supplyAPY, borrowAPY, Liquidity };
+  return { name, balance, supplyAPY, borrowAPY, liquidity, tvl };
 };
 
-const rows = [createData('WETH', 159, 6.0, 24, 4.0)];
+const rows = [createData('WETH', 1, 159, 6.0, 24, 4.0)];
 
 const ERC20Table = () => {
+  const { account, signer } = useWeb3Context();
+  const [walletBalance, setWalletBalance] = useState('');
+  useEffect(() => {
+    const getBalance = async () => {
+      const wethContract = Erc20__factory.connect(WETH_ADDRESS, signer);
+      const walletNumber = await wethContract.balanceOf(account);
+      const ret = Number(ethers.utils.formatEther(walletNumber)).toFixed(2);
+      setWalletBalance(ret);
+    };
+    getBalance();
+  }, [account, signer]);
   return (
     <TableContainer component={Paper}>
       <Typography variant="h6" component="div" sx={{ m: 3 }}>
@@ -46,6 +63,7 @@ const ERC20Table = () => {
         <TableHead>
           <TableRow>
             <TableCell>Assets</TableCell>
+            <TableCell align="right">Wallet Balance</TableCell>
             <TableCell align="right">TVL</TableCell>
             <TableCell align="right">Supply APY</TableCell>
             <TableCell align="right">Borrow APY</TableCell>
@@ -60,10 +78,11 @@ const ERC20Table = () => {
                 <TokenIcon name={row.name} />
                 {row.name}
               </IconWrap>
-              <TableCell align="right">{row.balance}</TableCell>
+              <TableCell align="right">{walletBalance}WETH</TableCell>
+              <TableCell align="right">{row.tvl}</TableCell>
               <TableCell align="right">{row.supplyAPY}</TableCell>
               <TableCell align="right">{row.borrowAPY}</TableCell>
-              <TableCell align="right">{row.Liquidity}</TableCell>
+              <TableCell align="right">{row.liquidity}</TableCell>
               <TableCell align="right">
                 <Button variant="outlined" size="small">
                   Borrow
--- src/static/constants/contract.ts
+++ src/static/constants/contract.ts
@@ -1,2 +1,3 @@
 export const UNIV3_ADDRESS = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
+export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 export const RPC_HOST = 'http://localhost:8545';
